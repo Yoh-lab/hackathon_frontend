8c9d9eb813c6e943c511c98d2d873ea638a93edc
liu <snliu6102@gmail.com>
2024-06-03 22:22:26 +0900

    modify app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { Flex, Box } from "@chakra-ui/react";
+import { Text } from '@chakra-ui/react'
 import { Button, ButtonGroup } from '@chakra-ui/react'
 
 export default function Home() {
   return (
     <Flex direction="column" align="center" justify="center" minH="100vh" gap={20}>
-      <h1 className="text-center">酔っ払いメーター</h1>
-      <Button colorScheme='teal' size='md'>ルーレットスタート！</Button>
+      <Text fontSize='4xl'>酔っ払いメーター</Text>
+      <Button colorScheme='teal' size='md'>診断開始！</Button>
     </Flex>
   );
 }
