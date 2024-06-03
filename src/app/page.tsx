4ea3eeacc01af6037a22a02ba3b89af6aeb5071f
liu <snliu6102@gmail.com>
2024-06-03 16:25:55 +0900

    modify app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 "use client";
 
+import { Flex, Box } from "@chakra-ui/react";
 import { Button, ButtonGroup } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="flex flex-col justify-center items-center gap-4">
-        <h1>酔っ払いメーター</h1>
-        <Button colorScheme='teal' size='md'>ルーレットスタート！</Button>
-      </div>
-    </div>
+    <Flex direction="column" align="center" justify="center" minH="100vh" gap={20}>
+      <h1 className="text-center">酔っ払いメーター</h1>
+      <Button colorScheme='teal' size='md'>ルーレットスタート！</Button>
+    </Flex>
   );
 }
