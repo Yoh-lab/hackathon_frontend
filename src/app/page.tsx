626ce247f8710efe0ca875529b4b86e0440cd8b9
rei3matsuura <sc24981i@st.omu.ac.jp>
2024-06-07 18:14:43 +0900

    [add] chakra-ui/iconsを使えるようにした

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@
 import { Flex, Box } from "@chakra-ui/react";
 import { Text } from '@chakra-ui/react'
 import { Button, ButtonGroup } from '@chakra-ui/react'
+import { ArrowRightIcon } from '@chakra-ui/icons'
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <Flex direction="column" align="center" justify="center" minH="100vh" gap={20}>
       <Text fontSize='4xl'>酔っ払いメーター</Text>
-      <Link href="/roulette-screen"><Button colorScheme='teal' size='md'>診断開始！</Button></Link>
+      <Link href="/roulette-screen"><Button colorScheme='teal' size='md'>診断開始！<ArrowRightIcon /></Button></Link>
     </Flex>
   );
 }
